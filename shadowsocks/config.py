051172255b59b1b0750d26255274a8a065bbd857
balde-soul <CJHdream@outlook.com>
2020-05-18 09:17:06 +0800

    complete the shadowsocks config.py

--- shadowsocks/config.py
+++ shadowsocks/config.py
@@ -14,11 +14,13 @@ parser.add_argument('--server_config_path', '--scp', dest='ServerConfigPath', ty
 parser.add_argument('--server_service_dir', '--ssd', dest='ServerServiceDir', type=str, default='./server_service', action='store', help='the dir where server service saved to')
 
 parser.add_argument('--config_client', '--cc', dest='ConfigClient', action='store_true', help='generate the client config if this flag is et')
+parser.add_argument('--kcptun_client', '--kc', dest='KcptunClient', action='store_true', help='generate the kcptun client')
+parser.add_argument('--normal_client', '--nc', dest='NormalClient', action='store_true', help='generate the kcptun client')
 parser.add_argument('--client_local_port', '--clp', dest='ClientLocalPort', default=[], type=int, nargs='+', help='the ports shadowsocks client local port, which would be use in other app')
 parser.add_argument('--client_remote_port', '--crp', dest='ClientRemotePort', default=[], type=int, nargs='+', help='the ports which is called ServerPort in shadowsocks server')
-parser.add_argument('--client_remote_ip', '--cri', dest='ClientRemoteIp', type=str, default='', help='the ip which called ServerIp in shadowsocks server')
+parser.add_argument('--client_kcptun_local_port', '--cklp', dest='ClientKcptunLocalPort', default=[], type=int, nargs='+', help='the ports which called ClientLocalPort in kcptun client, only used in kcptun_client')
+parser.add_argument('--client_remote_ip', '--cri', dest='ClientRemoteIp', type=str, default='', help='the ip which called ServerIp in shadowsocks server use only in the normal_client')
 parser.add_argument('--client_config_path', '--ccp', dest='ClientConfigPath', type=str, default='./client_config', action='store', help='the path where the client config file saved to')
-parser.add_argument('--client_executable_dir', '--ced', dest='ClientExecutableDir', type=str, default='./cilent_executable', action='store', help='the dir where client executable saved to')
 parser.add_argument('--client_service_dir', '--csd', dest='ClientServiceDir', type=str, default='./client_service', action='store', help='the dir where client service saved to')
 
 options = parser.parse_args()
@@ -95,67 +97,123 @@ if options.ConfigServer is True:
     pass
 
 if options.ConfigClient is True:
-    assert len(options.ClientLocalPort) == len(options.ClientRemotePort)
-    assert options.ClientRemoteIp != ''
-    assert options.ClientExecutableDir != ''
-    assert options.ClientConfigPath != ''
-    base_client_config = {
-        "localaddr": ":18887",
-        "remoteaddr": "target_ip:18887"
-    }
-    base_client_config.update(common_config)
+    if options.NormalClient is True:
+        assert len(options.ClientLocalPort) == len(options.ClientRemotePort)
+        assert options.ClientRemoteIp != ''
+        assert options.ClientConfigPath != ''
+        assert options.ClientServiceDir != ''
+        assert len(options.ClientLocalPort) != 0
+        assert len(options.ClientRemotePort) != 0
+        base_client_config = {
+            "server": "server",
+            "server_port": None,
+            "local_port": None
+        }
 
-    client_executable_path = os.path.join(options.ClientExecutableDir, 'client_linux_amd64')
-    print('client_executable_path: {0}'.format(client_executable_path))
-    base_client_service = \
-        "[Unit] \n" \
-        "Description=Kcptun Client {2}\n" \
-        "After=network.target \n" \
-        "[Service] \n" \
-        "ExecStart={0} -c {1}\n" \
-        "[Install] \n" \
-        "WantedBy=multi-user.target \n".format(os.path.abspath(client_executable_path), '{0}', '{1}')
+        base_client_config.update(client_common_config)
+        base_client_config.update(common_config)
+        base_client_config['server'] = options.ClientRemoteIp
 
-    print('clinet executable save to: {0}'.format(options.ClientExecutableDir))
-    if os.path.exists(options.ClientExecutableDir) is False:
-        print('mkdir: {0}'.format(options.ClientExecutableDir))
-        os.mkdir(options.ClientExecutableDir) if options.Test is False else None
-        shutil.copy('./client_linux_amd64', options.ClientExecutableDir) if options.Test is False else None
-        pass
+        base_client_service = \
+            "[Unit] \n" \
+            "Description=shadowsocks Client {1}\n" \
+            "After=network.target \n" \
+            "[Service] \n" \
+            "ExecStart=/usr/local/bin/sslocal -c {0}\n" \
+            "[Install] \n" \
+            "WantedBy=multi-user.target \n".format('{0}', '{1}')
 
-    print('client_config save to: {0}'.format(options.ClientConfigPath))
-    if os.path.exists(options.ClientConfigPath) is False:
-        print('mkdir: {0}'.format(options.ClientConfigPath))
-        os.mkdir(options.ClientConfigPath) if options.Test is False else None
-        pass
-
-    print('client_service save to: {0}'.format(options.ClientServiceDir ))
-    if os.path.exists(options.ClientServiceDir) is False:
-        print('mkdir: {0}'.format(options.ClientServiceDir))
-        os.mkdir(options.ClientServiceDir) if options.Test is False else None
-        pass
-
-    for lp, rp in zip(options.ClientLocalPort, options.ClientRemotePort):
-        base_client_config['localaddr'] = ':{0}'.format(lp)
-        base_client_config['remoteaddr'] = '{0}:{1}'.format(options.ClientRemoteIp, rp)
-
-        def save_config_to_json(client_config_file_path, config):
-            with open(client_config_file_path, 'w') as fp:
-                json.dump(config, fp, indent=4)
+        print('client_config save to: {0}'.format(options.ClientConfigPath))
+        if os.path.exists(options.ClientConfigPath) is False:
+            print('mkdir: {0}'.format(options.ClientConfigPath))
+            os.mkdir(options.ClientConfigPath) if options.Test is False else None
             pass
-        client_config_file_path = os.path.join(options.ClientConfigPath, 'shadowsocks-client-config-{0}.json'.format(lp))
-        print('client_config save to {0}'.format(client_config_file_path))
-        save_config_to_json(client_config_file_path, base_client_config) if options.Test is False else None
 
-        def save_client_service(client_service_file_path, client_service):
-            with open(client_service_file_path, 'w') as fp:
-                fp.write(client_service)
+        print('client_service save to: {0}'.format(options.ClientServiceDir ))
+        if os.path.exists(options.ClientServiceDir) is False:
+            print('mkdir: {0}'.format(options.ClientServiceDir))
+            os.mkdir(options.ClientServiceDir) if options.Test is False else None
+            pass
+
+        for lp, rp in zip(options.ClientLocalPort, options.ClientRemotePort):
+            base_client_config['local_port'] = ':{0}'.format(lp)
+            base_client_config['server_port'] = '{0}'.format(rp)
+
+            def save_config_to_json(client_config_file_path, config):
+                with open(client_config_file_path, 'w') as fp:
+                    json.dump(config, fp, indent=4)
                 pass
+            client_config_file_path = os.path.join(options.ClientConfigPath, 'shadowsocks-client-config-{0}.json'.format(lp))
+            print('client_config save to {0}'.format(client_config_file_path))
+            save_config_to_json(client_config_file_path, base_client_config) if options.Test is False else None
+
+            def save_client_service(client_service_file_path, client_service):
+                with open(client_service_file_path, 'w') as fp:
+                    fp.write(client_service)
+                    pass
+                pass
+            client_service = base_client_service.format(os.path.abspath(client_config_file_path), lp)
+            client_service_file_path = os.path.join(options.ClientServiceDir, 'shadowsocks-client-{0}.service'.format(lp))
+            print('client_service save to {0}'.format(client_service_file_path))
+            save_client_service(client_service_file_path, client_service) if options.Test is False else None
             pass
-        client_service = base_client_service.format(os.path.abspath(client_config_file_path), lp)
-        client_service_file_path = os.path.join(options.ClientServiceDir, 'shadowsocks-client-{0}.service'.format(lp))
-        print('client_service save to {0}'.format(client_service_file_path))
-        save_client_service(client_service_file_path, client_service) if options.Test is False else None
+        pass
+    if options.KcptunClient is True:
+        assert len(options.ClientLocalPort) == len(options.ClientKcptunLocalPort)
+        assert options.ClientConfigPath != ''
+        assert options.ClientServiceDir != ''
+        base_client_config = {
+            "server": "127.0.0.1",
+            "server_port": None,
+            "local_port": None
+        }
+
+        base_client_config.update(client_common_config)
+        base_client_config.update(common_config)
+
+        base_client_service = \
+            "[Unit] \n" \
+            "Description=shadowsocks Client use kcptun {1}\n" \
+            "After=network.target \n" \
+            "[Service] \n" \
+            "ExecStart=/usr/local/bin/sslocal -c {0}\n" \
+            "[Install] \n" \
+            "WantedBy=multi-user.target \n".format('{0}', '{1}')
+
+        print('client_config save to: {0}'.format(options.ClientConfigPath))
+        if os.path.exists(options.ClientConfigPath) is False:
+            print('mkdir: {0}'.format(options.ClientConfigPath))
+            os.mkdir(options.ClientConfigPath) if options.Test is False else None
+            pass
+
+        print('client_service save to: {0}'.format(options.ClientServiceDir ))
+        if os.path.exists(options.ClientServiceDir) is False:
+            print('mkdir: {0}'.format(options.ClientServiceDir))
+            os.mkdir(options.ClientServiceDir) if options.Test is False else None
+            pass
+
+        for lp, cklp in zip(options.ClientLocalPort, options.ClientKcptunLocalPort):
+            base_client_config['local_port'] = ':{0}'.format(lp)
+            base_client_config['server_port'] = '{0}'.format(cklp)
+
+            def save_config_to_json(client_config_file_path, config):
+                with open(client_config_file_path, 'w') as fp:
+                    json.dump(config, fp, indent=4)
+                pass
+            client_config_file_path = os.path.join(options.ClientConfigPath, 'shadowsocks-client-use-kcptun-config-{0}.json'.format(lp))
+            print('client_config save to {0}'.format(client_config_file_path))
+            save_config_to_json(client_config_file_path, base_client_config) if options.Test is False else None
+
+            def save_client_service(client_service_file_path, client_service):
+                with open(client_service_file_path, 'w') as fp:
+                    fp.write(client_service)
+                    pass
+                pass
+            client_service = base_client_service.format(os.path.abspath(client_config_file_path), lp)
+            client_service_file_path = os.path.join(options.ClientServiceDir, 'shadowsocks-client-use-kcptun-{0}.service'.format(lp))
+            print('client_service save to {0}'.format(client_service_file_path))
+            save_client_service(client_service_file_path, client_service) if options.Test is False else None
+            pass
         pass
     pass
 
---
+++ shadowsocks/example_config.md
@@ -0,0 +1,8 @@
+config the server:
+python config.py --config_server --server_config_path=/home/sins/shadowsocks --server_service_dir=/home/sins/shadowsocks --server_ip=182.0.0.1 --server_port=18888 18890
+
+config the normal client:
+python config.py --config_client --normal_client --client_config_path=/home/sins/shadowsocks --client_service_dir=/home/sins/shadowsocks --client_local_port 18888 18890 --client_remote_port 18888 18890 --client_remote_ip=182.0.0.1
+
+config the kcptun client:
+python config.py --config_cliten --kcptun_client --client_config_path=/home/sins/shadowsocks --client_service_dir=/home/sins/shadowsocks --client_local_port 18888 18890 --client_kcptun_local_port 18887 18889
