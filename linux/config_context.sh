b28fc2e080f2ae88195d8394edbe831be29fe974
jihuacao <CJHdream@outlook.com>
2022-08-08 17:21:54 +0800

    udpate

--- linux/config_context.sh
+++ linux/config_context.sh
@@ -1,22 +1,35 @@
 ﻿ConfigCCPP=0
 CmakeVersion=3.19.0
 usage(){
-    echo 'help message'
+    echo "help message"
+    echo "--nvidia: config nvidia context while set"
+    echo "--nvidia-cuda-version set the cuda version"
+    echo "--nvidia-cudnn-version set the cudnn version"
+    echo "--nvidia-nccl"
 }
 ARGS=`getopt \
     -o h \
     --long help, \
     --long config_c_cpp \
-    --long cmake_version
+    --long cmake_version:: \
+    --long nvidia \
+    --long nvidia-cuda-version:: \
+    --long nvidia-cudnn-version:: \
+    --long nvidia-nccl \
+    --long nvidia-tensort \
     -n 'example.bash' -- "$@"`
 if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
 eval set -- "${ARGS}"
 while true ; do
     case "$1" in
         --config_c_cpp)
-            echo "config c_cpp evnironment"; ConfigCCPP=1; shift 1; break ;;
+            echo "config c_cpp evnironment"; ConfigCCPP=1; shift 1 ;;
         --cmake_version)
-            echo "cmake version: $2"; CmakeVersion=$2; shift 2; break ;;
+            echo "cmake version: $2"; CmakeVersion=$2; shift 2 ;;
+        --nvidia)
+            echo "config nvidia context"; ConfigNvidia=1; shift 1 ;; 
+        --nvidia-cuda-version)
+            echo ""
         -h|--help)
             usage; exit 1 ;;
         --)
@@ -37,3 +50,5 @@ wget https://github.com/Kitware/CMake/releases/download/v${CmakeVersion}/cmake-$
 while 1; do
     wget -c https://github.com/Kitware/CMake/releases/download/v${CmakeVersion}/cmake-${CmakeVersion}-Linux-x86_64.sh
 done
+
+wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda_10.2.89_440.33.01_linux.run
---
+++ linux/docker-in/python.Dockerfile
@@ -0,0 +1,28 @@
+RUN \
+echo "python envs" \
+&& apt install software-properties-common \
+&& add-apt-repository ppa:deadsnakes/ppa \
+&& apt -y install python3.9 \
+&& apt -y install python-pip \
+&& echo "done"
+
+RUN \
+--mount=type=cache,target=/root/.cache \
+#--mount=type=bind,target=/root/.cache/pip,source=DockerContext/pip-cache,rw \
+mkdir ~/.pip \
+#&& cp /root/.cache/pip/test.md ~/ \
+## 换源
+&& echo '[global]' >> ~/.pip/pip.conf \
+&& echo 'index-url=http://mirrors.aliyun.com/pypi/simple/' >> ~/.pip/pip.conf \
+&& echo 'extra-index-url=http://pypi.mirrors.ustc.edu.cn/simple/' >> ~/.pip/pip.conf \
+&& echo '                http://pypi.douban.com/simple/' >> ~/.pip/pip.conf \
+&& echo '                https://pypi.tuna.tsinghua.edu.cn/simple/' >> ~/.pip/pip.conf \
+## 修改缓存地址
+&& echo '[install]' >> ~/.pip/pip.conf \
+&& echo 'trusted-host=mirrors.aliyun.com' >> ~/.pip/pip.conf \
+&& echo '             pypi.mirrors.ustc.edu.cn' >> ~/.pip/pip.conf \
+&& echo '             pypi.douban.com' >> ~/.pip/pip.conf \
+&& echo '             pypi.tuna.tsinghua.edu.cn' >> ~/.pip/pip.conf \
+## 升级pip
+&& pip install --upgrade pip \
+&& echo 'end'
