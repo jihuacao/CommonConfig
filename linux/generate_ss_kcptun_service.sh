a39c96a1b2f88ea6cc6a1b9ebb19d504802ff714
root <root@vultr.guest>
2023-01-07 16:17:55 +0000

    update

--- linux/generate_ss_kcptun_service.sh
+++ linux/generate_ss_kcptun_service.sh
@@ -10,12 +10,14 @@ pip install --upgrade pip
 pip install shadowsocks==2.8.2
 sed -i 's/EVP_CIPHER_CTX_cleanup/EVP_CIPHER_CTX_reset/' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
 
-now=$(pwd) && cd linux/shadowsocks && python config.py --config_server --server_config_path=/root/ss --server_service_dir=/root/ss --server_port ${ssPort} --server_ip=${IP} && cd ${now}
-now=$(pwd) && cd linux/kcptun && python config.py --config_server --server_executable_dir=/root/kcptun --server_config_path=/root/kcptun --server_service_dir=/root/kcptun --server_target_port ${ssPort} --server_listen_port ${kcptunPort} --server_target_ip=${IP} && cd ${now}
+rm -rf ${HOME}/ss-service/
+mkdir -p ${HOME}/ss-service/
+now=$(pwd) && cd linux/shadowsocks && python config.py --config_server --server_config_path=/root/ss --server_service_dir=${HOME}/ss-service --server_port ${ssPort} --server_ip=${IP} && cd ${now}
+now=$(pwd) && cd linux/kcptun && python config.py --config_server --server_executable_dir=/root/kcptun --server_config_path=${HOME}/kcptun-service --server_service_dir=/root/kcptun --server_target_port ${ssPort} --server_listen_port ${kcptunPort} --server_target_ip=${IP} && cd ${now}
 
-cp /root/ss/shadowsocks-server.service /etc/systemd/system/
-cp /root/kcptun/kcptun-server-${kcptunPort}.service /etc/systemd/system/
-chmod +x /root/kcptun/server_linux_amd64
+cp ${HOME}/ss-service/shadowsocks-server.service /etc/systemd/system/
+cp ${HOME}/kcptun-service/kcptun-server-${kcptunPort}.service /etc/systemd/system/
+chmod +x ${HOME}/kcptun-service/server_linux_amd64
 
 systemctl daemon-reload
 systemctl enable shadowsocks-server.service
